b86d303ea764904675870679aed704579ea4c818
dmitry <my@my.ru>
2020-03-01 01:58:37 +0300

    - draw mini map

--- src/main/kotlin/core/GameDrawer.kt
+++ src/main/kotlin/core/GameDrawer.kt
@@ -2,16 +2,43 @@ package core
 
 import org.w3c.dom.CanvasRenderingContext2D
 
+private const val MINI_MAP_TILE_SIZE = 8
+
 class GameDrawer(val game: Game) {
-    fun drawGame(ctx: CanvasRenderingContext2D) {
-
-        drawLevelMap(ctx)
+    lateinit var ctx: CanvasRenderingContext2D
+    fun drawGame(curCtx: CanvasRenderingContext2D) {
+        ctx = curCtx
+        drawLevelMap()
     }
 
-    private fun drawLevelMap(ctx: CanvasRenderingContext2D) {
-        ctx.fillStyle = "#FFFF22";
-        ctx.fillRect(0.0, 0.0, 150.0, 75.0);
-        ctx.fillStyle = "#FF33FF";
-        ctx.fillRect(0.0, 0.0, 130 * 1.0, 55.0);
+    private fun drawLevelMap() {
+        ctx.clearRect(0.0, .0, ctx.canvas.width.toDouble(), ctx.canvas.height.toDouble())
+
+        
+        drawMiniMap()
     }
+
+    private fun drawMiniMap() {
+        game.level.map.fori { x, y, v ->
+            when (v) {
+                Tile.WALL -> {
+                    drawTileWall(x, y)
+                }
+                else -> {
+                }
+            }
+        }
+
+        ctx.fillStyle = "#55ff1011";
+        ctx.fillRect(0.0, .0, MINI_MAP_TILE_SIZE * MAP_SIZE.toDouble(), MINI_MAP_TILE_SIZE * MAP_SIZE.toDouble())
+    }
+
+    private fun drawTileWall(x: Int, y: Int) {
+        ctx.fillStyle = "#000";
+        ctx.fillRect(x * MINI_MAP_TILE_SIZE, y * MINI_MAP_TILE_SIZE, MINI_MAP_TILE_SIZE, MINI_MAP_TILE_SIZE);
+    }
+}
+
+private fun CanvasRenderingContext2D.fillRect(x: Int, y: Int, w: Int, h: Int) {
+    fillRect(x.toDouble(), y.toDouble(), w.toDouble(), h.toDouble())
 }
